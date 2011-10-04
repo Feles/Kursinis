e8cd136887c44f78c2911d16f9d83075e93f08ca
Laurynas <lowreens@gmail.com>
2011-10-04 22:28:08 +0300

    Duomenys, ignore list, grafikai

---
+++ grafikai.r
@@ -0,0 +1,22 @@
+rm(list = ls())
+
+grafikai = function(a)
+{
+pav = colnames(a)
+plot(a)
+savePlot(filename = paste(pav, " normal.wmf"), type = "wmf", device = dev.cur(), restoreConsole = TRUE)
+plot(log(a))
+savePlot(filename = paste(pav, " log.wmf"), type = "wmf", device = dev.cur(), restoreConsole = TRUE)
+}
+
+vki = ts(read.csv2('data/vki.csv'), start = 1991, frequency = 12)
+pal = ts(read.csv2('data/pal.csv'), start = 2004:10, frequency = 12)
+min = ts(read.csv2('data/min alga.csv'), start = 1995, frequency = 12)
+iki = ts(read.csv2('data/iki.csv'), start = 2006, frequency = 12)
+oil = ts(read.csv2('data/oil.csv'), start = 2006, frequency = 12)
+
+grafikai(vki)
+grafikai(pal)
+grafikai(min)
+grafikai(iki)
+grafikai(oil)
