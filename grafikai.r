40fa5cce7ed298617c4d0ce67fb48b01d5e30ecb
Laurynas <lowreens@gmail.com>
2011-10-09 22:14:13 +0300

    Grafikai

--- grafikai.r
+++ grafikai.r
@@ -2,23 +2,23 @@ rm(list = ls())
 
 grafikai = function(a)
 {
-tipas = "wmf" 
+tipas = "jpeg" 
 
 pav = colnames(a)
 plot(a)
-savePlot(filename = paste(pav, " normal.",tipas), type = tipas, device = dev.cur(), restoreConsole = TRUE)
+savePlot(filename = paste(pav, " normal.",tipas, sep=""), type = tipas, device = dev.cur(), restoreConsole = TRUE)
 plot(log(a))
-savePlot(filename = paste(pav, " log.",tipas), type = tipas, device = dev.cur(), restoreConsole = TRUE)
+savePlot(filename = paste(pav, " log.",tipas,  sep=""), type = tipas, device = dev.cur(), restoreConsole = TRUE)
 }
 
-vki = ts(read.csv2('data/vki.csv'), start = 1991, frequency = 12)
-pal = ts(read.csv2('data/pal.csv'), start = 2004:10, frequency = 12)
-min = ts(read.csv2('data/min alga.csv'), start = 1995, frequency = 12)
-iki = ts(read.csv2('data/iki.csv'), start = 2006, frequency = 12)
-oil = ts(read.csv2('data/oil.csv'), start = 2006, frequency = 12)
+vki = window(ts(read.csv2('data/vki.csv'), start = 1991, frequency = 12) ,start = 2006, freq = 12)
+pal = window(ts(read.csv2('data/pal.csv'), start = c(2004, 10), frequency = 12) ,start = 2006, freq = 12)
+min = window(ts(read.csv2('data/min alga.csv'), start = 1995, frequency = 12),start = 2006, freq = 12)
+#iki = ts(read.csv2('data/iki.csv'), start = 2006, frequency = 12)
+#oil = ts(read.csv2('data/oil.csv'), start = 2006, frequency = 12)
 
 grafikai(vki)
 grafikai(pal)
 grafikai(min)
-grafikai(iki)
-grafikai(oil)
+#grafikai(iki)
+#grafikai(oil)
