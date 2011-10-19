2cdf05e439c28820e08a0b29d2137233fee7546d
Daiva Kemzūraitė <daivakemz@gmail.com>
2011-10-20 01:29:40 +0300

    Update grafikai.r

--- grafikai.r
+++ grafikai.r
@@ -1,55 +1,57 @@
-grafikai = function(a, tipas = 'pdf', ...)
-{
-pav = colnames(a)
+ grafikai = function(a, tipas = 'pdf', ...)
+ {
+ pav = colnames(a)
 
-plot(a, xlab = 'laikas')
-savePlot(filename = paste('grafikai//',pav, ' lin.',tipas, sep=''), type = tipas, device = dev.cur(), restoreConsole = TRUE)
+ plot(a, xlab = 'laikas')
+ savePlot(filename = paste('grafikai//',pav, ' lin.',tipas, sep=''), type = tipas, device = dev.cur(), restoreConsole = TRUE)
 
-plot(log(a), xlab = 'laikas' , ylab = paste('log',pav))
-savePlot(filename = paste('grafikai//',pav, ' log.',tipas,  sep=''), type = tipas, device = dev.cur(), restoreConsole = TRUE)
+ plot(log(a), xlab = 'laikas' , ylab = paste('log',pav))
+ savePlot(filename = paste('grafikai//',pav, ' log.',tipas,  sep=''), type = tipas, device = dev.cur(), restoreConsole = TRUE)
 
-plot(decompose(vki), xlab = 'laikas')
-savePlot(filename = paste('grafikai//',pav, ' decomposed.',tipas,  sep=''), type = tipas, device = dev.cur(), restoreConsole = TRUE)
-}
+ plot(decompose(vki), xlab = 'laikas')
+ savePlot(filename = paste('grafikai//',pav, ' decomposed.',tipas,  sep=''), type = tipas, device = dev.cur(), restoreConsole = TRUE)
+ }
 
 #kol kas neturime disponuojamu menesiniu gyventoju pajamu,- jas itrauksime veliau
-grafikai(vki)
-grafikai(pal)
-grafikai(iki)
-grafikai(oil)
+ grafikai(vki)
+ grafikai(pal)
+ grafikai(iki)
+ grafikai(oil)
 
 #tikriname, ar komponentes turi trenda
 
-trend.test(vki)
-trend.test(pal)  
-trend.test(iki)
-trend.test(oil)
+ trend.test(vki)
+ trend.test(pal)  
+ trend.test(iki)
+ trend.test(oil)
 
 #grafikai (perkelti i grafiku brezimo funkcija)
 
-plot(decompose(vki))
-plot(decompose(pal))
-plot(decompose(iki))
-plot(decompose(oil))
+ plot(decompose(vki))
+ plot(decompose(pal))
+ plot(decompose(iki))
+ plot(decompose(oil))
 
 #sezoniskumo panaikinimas
 
-vki_s<-vki-decompose(vki)$seasonal
-pal_s<-pal-decompose(pal)$seasonal 
-iki_s<-iki-decompose(iki)$seasonal
-oil_s<-oil-decompose(oil)$seasonal
+ vki_s<-vki-decompose(vki)$seasonal
+ pal_s<-pal-decompose(pal)$seasonal 
+ iki_s<-iki-decompose(iki)$seasonal
+ oil_s<-oil-decompose(oil)$seasonal
 
-loil_s.df<-ur.df(log(oil_s),lags=5,selectlags="BIC",type="trend")
-summary(loil_s.df)
+ loil_s.df<-ur.df(log(oil_s),lags=5,selectlags="BIC",type="trend")
+ summary(loil_s.df)
 
 #kai panaikiname sezoniskuma, visos komponentes turi vienetines saknis, norint sudaryti tiesini modeli, dar reikia atlikti kointegravimo testus
 
-#modelis1<-lm(log(vki_s)~log(pal_s)+log(iki_s)+log(oil_s))
-#summary(modelis1)
+ modelis1<-lm(log(vki_s)~log(pal_s)+log(iki_s)+log(oil_s))
+ summary(modelis1)
+
+#(gero modelio liekanos sudaro BT), siuo atveju liekanos nera stacionarios
+
+ tsdisplay(summary(modelis1)$res)
 
 #atliekame kointegravimo testus su -4.96 kritine reiksme
-
- library(tseries) 
 
  m<-lm(vki_s~iki_s)
  beta<-coef(m)
