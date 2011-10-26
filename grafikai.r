f163ae80e9b972c8e8c576e7d6def1c8ad656ad6
Daiva Kemzūraitė <daivakemz@gmail.com>
2011-10-27 00:25:06 +0300

    Update grafikai.r

--- grafikai.r
+++ grafikai.r
@@ -12,18 +12,13 @@
  savePlot(filename = paste('grafikai//',pav, ' decomposed.',tipas,  sep=''), type = tipas, device = dev.cur(), restoreConsole = TRUE)
  }
 
-#kol kas neturime disponuojamu menesiniu gyventoju pajamu,- jas itrauksime veliau
+
  grafikai(vki)
  grafikai(pal)
  grafikai(iki)
  grafikai(oil)
+ grafikai(pk)
 
-#tikriname, ar komponentes turi trenda
-
- trend.test(vki)
- trend.test(pal)  
- trend.test(iki)
- trend.test(oil)
 
 #grafikai (perkelti i grafiku brezimo funkcija)
 
@@ -31,6 +26,7 @@
  plot(decompose(pal))
  plot(decompose(iki))
  plot(decompose(oil))
+ plot(decompose(pk))
 
 #sezoniskumo panaikinimas
 
@@ -38,6 +34,14 @@
  pal_s<-pal-decompose(pal)$seasonal 
  iki_s<-iki-decompose(iki)$seasonal
  oil_s<-oil-decompose(oil)$seasonal
+ pk_s<-pk-decompose(pk)$seasonal
+
+ lvki_s<-log(vki_s)
+ lpal_s<-log(pal_s)
+ loil_s<-log(oil_s)
+ liki_s<-log(iki_s)
+ lpk_s<-log(pk_s)
+ 
 
  loil_s.df<-ur.df(log(oil_s),lags=5,selectlags="BIC",type="trend")
  summary(loil_s.df)
@@ -69,3 +73,29 @@
  adf.test(sprd2, alternative="stationary", k=3)
 
 #visais atvejais liekanos turi vienetine sakni, todel reikes isdiferencijuoti ir modeli sudaryti skirtumams
+
+#modelis
+
+#Modelio sudarymas
+
+ tt <- time(vki)
+ D <- ifelse(tt>=2009,1,0)
+ 
+ mod<-dynlm(lvki_s~D+tt+tt*D+L(lpal_s,0:1)+L(loil_s,0:1)+L(liki_s,0:1)+L(lpk_s,0:1))
+ summary(mod)
+
+#panaikiname nereikšmingus kintamuosius
+ mod<-dynlm(lvki_s~D+tt+tt*D+L(liki_s,1)+L(lpk_s,0:1))
+ summary(mod)
+
+#kointegravimo tikrinimas
+ mod.res<-ts(mod$res,start=2006,freq=12)
+ summary(dynlm(d(mod.res)~-1+L(mod.res),1:12))
+#taigi su -3.34 kritine reiksme gauname, jog musu modelis yra kointegruotas procesas
+
+#korekcijos modelis
+ mod_ecm<-dynlm(d(lvki_s)~-1+L(mod.res)+L(d(lpal_s),0:1)+L(d(loil_s),0:1)+L(d(liki_s),0:1)+L(d(lpk_s),0:1))
+ summary(mod_ecm)
+#visi kintamieji yra nereikšmingi.
+
+#Tirsime kitą modelį!
